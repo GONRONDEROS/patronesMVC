240d0fb7aedbc774f779a2a17aa2b092c634b7ed
Gonzalo Ronderos <gonzalorond@gmail.com>
2022-02-18 15:46:11 -0300

    Agregue el ruteo para home y about

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -2,11 +2,16 @@ const path = require("path");
 
 const mainController = {
     //Funcion que devuelve una rta para la pagina home//
-    index: (req, res) => {
+    home: (req, res) => {
         const fileToSend = path.join(__dirname, "../views/home.html");
 
     res.sendFile(fileToSend);
     },
+    about: (req, res) => {
+        const fileToSend = path.join(__dirname, "../views/about.html");
+
+    res.sendFile(fileToSend);
+    }
 };
 
 //exportamos el objeto controlador
--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/mainControllers")
 
-//Si la solicitud es GET y la ruta "/" llamamos a la funcion index de mainController//
-router.get("/", mainController.index);
+//Si la solicitud es GET y la ruta "/" llamamos a la funcion home de mainController//
+router.get("/", mainController.home);
+
+//Si la solicitud es GET y la ruta "/" llamamos a la funcion about de mainController//
+router.get("/about", mainController.about);
 
 //exportamos el metodo router para que pueda ser usado en nuestra app
 module.exports = router;
